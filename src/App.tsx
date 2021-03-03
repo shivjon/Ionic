ed91e82d5e6566b9cb770f6d7a01fe261daa1619
haninder <haninder@crossml.com>
2021-03-03 20:32:22 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React, {useRef, useState} from 'react';
+import { IonApp, IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRow, IonTitle, IonToolbar, IonCardHeader, IonCardSubtitle, IonCardTitle, IonAlert } from '@ionic/react';
+import { logIn, person,  } from "ionicons/icons";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,19 +21,105 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
+const App: React.FC = () => {
+
+  const name = useRef<HTMLIonInputElement>(null);
+  const lastName = useRef<HTMLIonInputElement>(null);
+  const [nameFirst, setNameFirst] = useState<string>('');
+  const [showAlert1, setShowAlert1] = useState(false);
+
+  const getName = () =>{
+    var value= name.current?.value;
+    var val= lastName.current?.value;
+    setShowAlert1(true)
+  }
+  const arry = [
+    {name:"s"},
+    {name:"s"},
+    {name:"s"},
+    {name:"s"}
+
+  ]
+
+return (
+  <>
+       <IonAlert
+          isOpen={showAlert1}
+          onDidDismiss={() => setShowAlert1(false)}
+          cssClass='my-custom-class'
+          header={'Alert'}
+          subHeader={'Subtitle'}
+          message={'This is an alert message.'}
+          buttons={['OK']}
+        />
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>
+          Yewwo Merchant
+        </IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className="ion-padding">
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+           <IonItem>
+            <IonIcon slot="start" icon={person}  />
+            <IonLabel position="floating">Name</IonLabel>
+            <IonInput ref={name}>
+            </IonInput>
+           </IonItem>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+           <IonItem>
+           <IonIcon slot="start" icon={person}  />
+            <IonLabel position="floating">Last Name</IonLabel>
+            <IonInput ref={lastName} onIonChange={(e)=>{setNameFirst(e.detail.value!)}} >
+            </IonInput>
+           </IonItem>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol className="ion-text-left">
+            <IonButton  onClick={getName}>
+              <IonIcon  slot="start" icon={logIn}/>
+              Login
+            </IonButton>
+          </IonCol>
+          <IonCol className="ion-text-right">
+            <IonButton onClick={getName}>
+              <IonIcon slot="start" icon={logIn}/>
+              Registration
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        {nameFirst && (
+        <IonRow>
+          <IonCol>
+            <IonCard>
+              { arry.map((item,index)=>(
+                <IonCardHeader key={index}>
+                <IonCardSubtitle>{item.name}</IonCardSubtitle>
+                <IonCardTitle>Card Title</IonCardTitle>
+                </IonCardHeader>
+              )) }
+           
+            <IonCardContent>
+              Keep close to Nature's heart... and break clear away, once in awhile,
+              and climb a mountain or spend a week in the woods. Wash your spirit clean.
+            </IonCardContent>
+          </IonCard>
+          </IonCol>
+        </IonRow>
+        )}
+      </IonGrid>
+    </IonContent>
   </IonApp>
+  </>
 );
+}
 
 export default App;
